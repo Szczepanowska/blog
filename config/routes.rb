bb56363d44f8022aea31669fc8aa04568a8e12d0
Natalia Szczepanowska <szczepanowska.n@gmail.com>
2022-10-25 11:13:02 +0200

    sigining up

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
 
   resources :articles
+  resources :users, except: [:new]
 end
